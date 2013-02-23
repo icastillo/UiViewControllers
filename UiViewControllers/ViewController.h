90ae7eff9ed31788b57197fe868c5a3253f0a1ca
isaias castillo <isaias@MacBook-Air-de-isaias.local>
2013-02-23 01:08:47 -0500

    Initial Commit

---
+++ UiViewControllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UiViewControllers
+//
+//  Created by isaias castillo on 23/02/13.
+//  Copyright (c) 2013 isaias castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ UiViewControllers/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UiViewControllers
+//
+//  Created by isaias castillo on 23/02/13.
+//  Copyright (c) 2013 isaias castillo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
